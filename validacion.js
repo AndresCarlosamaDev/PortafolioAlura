22a3f217ee9577a73f29cb29bcf50165d32c4143
AndresCarlosamaDev <andrescarlosdev@gmail.com>
2023-06-28 22:30:18 -0500

    Sepacion CSS

--- validacion.js
+++ validacion.js
@@ -1,1 +1,31 @@
-//Haz tú validación en javascript acá
+const inputs = document.querySelectorAll('input');
+const txtArea = document.getElementById('mensagem');
+
+inputs.forEach((input) => {
+    input.addEventListener('blur', (input) => {
+        validar(input.target);
+    });
+});
+
+txtArea.addEventListener('blur', (input) => {
+    validarTextBox(input.target);
+});
+
+
+function validar(input) {
+    if (input.validity.valid) {
+        input.classList.remove('formcontato__input--error');
+    } else {
+        input.classList.add('formcontato__input--error');
+    }
+    
+}
+
+function validarTextBox(input) {
+    if (input.validity.valid) {
+        input.classList.remove('formcontato__textarea--error');
+    } else {
+        input.classList.add('formcontato__textarea--error');
+        
+    }
+}
